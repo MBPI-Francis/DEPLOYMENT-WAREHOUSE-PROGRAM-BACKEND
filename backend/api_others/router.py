fa9d5a7e5d7c56e3c606745f1b1f85203b43ebbd
FrancisIkoy-AECC <mbpi.francis@gmail.com>
2025-05-02 16:54:31 +0800

    ALTERED THE API FOR ADJUSTMENT FORM

--- backend/api_others/router.py
+++ backend/api_others/router.py
@@ -8,6 +8,7 @@ from uuid import UUID
 from backend.api_preparation_form.v1.models import TempPreparationForm
 from backend.api_notes.v1.models import TempNotes
 from backend.api_transfer_form.v1.models import TempTransferForm
+from backend.api_adjustment_form.v1.models import AdjustmentForm
 from backend.api_outgoing_report.v1.models import TempOutgoingReport
 from backend.api_receiving_report.v1.models import TempReceivingReport
 from backend.api_stock_on_hand.v1.models import StockOnHand
@@ -144,6 +145,7 @@ async def update_date_computed(db: get_db = Depends()):
         TempOutgoingReport,
         TempReceivingReport,
         TempHeldForm,
+        AdjustmentForm,
     ]
 
     updated_tables = []
@@ -289,6 +291,11 @@ async def clear_table_data(tbl: str, db: get_db = Depends()):
             TempHeldForm,
         ]
 
+    elif tbl == 'adjustment forms':
+        tables = [
+            AdjustmentForm,
+        ]
+
     elif tbl == 'all':
         tables = [
             TempNotes,
@@ -297,6 +304,7 @@ async def clear_table_data(tbl: str, db: get_db = Depends()):
             TempOutgoingReport,
             TempReceivingReport,
             TempHeldForm,
+            AdjustmentForm,
         ]
 
     updated_tables = []
