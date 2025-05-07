5e9640a3ccb4c680f6368e126ebb30effeb4e40f
FrancisIkoy-AECC <mbpi.francis@gmail.com>
2025-05-07 14:43:43 +0800

    updated the schema

--- backend/api_adjustment_form/v1/schemas.py
+++ backend/api_adjustment_form/v1/schemas.py
@@ -12,8 +12,8 @@ class AdjustmentForm(BaseModel):
     ref_number: str = Field(max_length=50, description="The reference number of the Adjustment Form")
     adjustment_date: date
     reference_date: date
-    ref_form: Optional[str] = Field(max_length=50, description="The referenced document of the adjustment")
-    ref_form_number: Optional[str] = Field(max_length=50, description="The referenced number of the referenced document")
+    ref_form: str = Field(max_length=50, description="The referenced document of the adjustment")
+    ref_form_number: str = Field(max_length=50, description="The referenced number of the referenced document")
     qty_kg: float
     status_id: UUID
     reason: str = Field(max_length=255, description="The reason for the adjustment")
