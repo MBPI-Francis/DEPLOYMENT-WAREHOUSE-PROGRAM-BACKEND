bd0996f1501f92cd3ee057cd40b96f9f151ce969
FrancisIkoy-AECC <mbpi.francis@gmail.com>
2025-05-30 17:04:06 +0800

    V8-IAF

--- setup_files/adjusment_form_computation_logc_views_V2.sql
+++ setup_files/adjusment_form_computation_logc_views_V2.sql
@@ -153,17 +153,19 @@ WITH ending_balance AS (
 	
 
 -- ---------------------------[Transfer Form Queries]---------------------------
-	
-	adf_transfer_from_correct AS (
-	     SELECT tac.from_warehouse_id AS warehouseid,
+	adf_transfer_correct AS (
+	     SELECT 
+		 	tac.from_warehouse_id AS wh_from_id,
+			tac.to_warehouse_id AS wh_to_id,
 	        tac.rm_code_id AS rawmaterialid,
-	        - sum(tac.qty_kg) AS transferred_from_qty,
+	        sum(tac.qty_kg) AS transferred_qty,
 	        tac.date_computed AS datecomputed,
 	        status.id AS statusid,
-	        status.name AS statusname
+	        status.name AS status
 	    FROM tbl_adjustment_correct tac
 	        JOIN tbl_transfer_forms tf ON tac.incorrect_transfer_id = tf.id
 	        JOIN tbl_warehouses wh_from ON tac.from_warehouse_id = wh_from.id
+			JOIN tbl_warehouses wh_to ON tac.to_warehouse_id = wh_to.id
 	        JOIN tbl_status status ON tac.status_id = status.id
 	
 	      WHERE (tac.is_cleared IS NULL OR tac.is_cleared = false)
@@ -174,86 +176,43 @@ WITH ending_balance AS (
 	                tac.rm_code_id,
 	                tac.date_computed,
 	                status.id,
-	                status.name
+	                status.name,
+					tac.to_warehouse_id,
+					tac.rm_code_id
 	),
 
 
+	adf_transfer_incorrect AS (
+	 SELECT 
+		tf.from_warehouse_id AS wh_from_id,
+		tf.to_warehouse_id AS wh_to_id,
+		tf.rm_code_id AS rawmaterialid,
+		sum(tf.qty_kg) AS transferred_qty,
+		tf.date_computed AS datecomputed,
+		status.id AS statusid,
+		status.name AS status
+	FROM tbl_adjustment_correct tac
+		JOIN tbl_transfer_forms tf ON tac.incorrect_transfer_id = tf.id
+		JOIN tbl_warehouses wh_from ON tf.from_warehouse_id = wh_from.id
+		JOIN tbl_warehouses wh_to ON tf.to_warehouse_id = wh_to.id
+		JOIN tbl_status status ON tf.status_id = status.id
 
-	adf_transfer_from_incorrect AS (
-    SELECT tf.from_warehouse_id AS warehouseid,
-        tf.rm_code_id AS rawmaterialid,
-        sum(tf.qty_kg) AS transferred_from_qty,
-        tf.date_computed AS datecomputed,
-        status.id AS statusid,
-        status.name AS statusname
-    FROM tbl_adjustment_correct tac
-        JOIN tbl_transfer_forms tf ON tac.incorrect_transfer_id = tf.id
-        JOIN tbl_warehouses wh_from ON tf.from_warehouse_id = wh_from.id
-        JOIN tbl_status status ON tf.status_id = status.id
-
-      WHERE (tac.is_cleared IS NULL OR tac.is_cleared = false)
-        AND (tac.is_deleted IS NULL
-        OR tac.is_deleted = false)
-        AND tac.date_computed IS NULL
-      GROUP BY 	tf.from_warehouse_id,
-                tf.rm_code_id,
-                tf.date_computed,
-                status.id,
-                status.name
+	  WHERE (tac.is_cleared IS NULL OR tac.is_cleared = false)
+		AND (tac.is_deleted IS NULL
+		OR tac.is_deleted = false)
+		AND tac.date_computed IS NULL
+	  GROUP BY 	tf.from_warehouse_id,
+				tf.rm_code_id,
+				tf.date_computed,
+				status.id,
+				status.name,
+				tf.to_warehouse_id,
+				tf.rm_code_id
 		),
 
 
-	adf_transfer_to_correct AS (
-    SELECT tac.to_warehouse_id AS warehouseid,
-        tac.rm_code_id AS rawmaterialid,
-        sum(tac.qty_kg) AS transferred_to_qty,
-        tac.date_computed AS datecomputed,
-        status.id AS statusid,
-        status.name AS statusname
-    FROM tbl_adjustment_correct tac
-    JOIN tbl_transfer_forms tf ON tac.incorrect_transfer_id = tf.id
-    JOIN tbl_warehouses wh_to ON tac.to_warehouse_id = wh_to.id
-    JOIN tbl_status status ON tac.status_id = status.id
-
-    WHERE (tac.is_cleared IS NULL OR tac.is_cleared = false)
-        AND (tac.is_deleted IS NULL
-        OR tac.is_deleted = false)
-        AND tac.date_computed IS NULL
-    GROUP BY tac.to_warehouse_id,
-        tac.rm_code_id,
-        tac.date_computed,
-        status.id,
-        status.name
-
-		),
 
 
-	adf_transfer_to_incorrect AS (
-    SELECT tf.to_warehouse_id AS warehouseid,
-        tf.rm_code_id AS rawmaterialid,
-        - sum(tf.qty_kg) AS transferred_to_qty,
-        tf.date_computed AS datecomputed,
-        status.id AS statusid,
-        status.name AS statusname
-    FROM tbl_adjustment_correct tac
-    JOIN tbl_transfer_forms tf ON tac.incorrect_transfer_id = tf.id
-    JOIN tbl_warehouses wh_to ON tf.to_warehouse_id = wh_to.id
-    JOIN tbl_status status ON tf.status_id = status.id
-
-    WHERE (tac.is_cleared IS NULL OR tac.is_cleared = false)
-        AND (tac.is_deleted IS NULL
-        OR tac.is_deleted = false)
-        AND tac.date_computed IS NULL
-    GROUP BY tf.to_warehouse_id,
-        tf.rm_code_id,
-        tf.date_computed,
-        status.id,
-        status.name
-
-		),
-
-	
-	
 -- ---------------------------[Change Status Form Queries]---------------------------
 
 	adf_change_status_correct AS (
@@ -261,6 +220,10 @@ WITH ending_balance AS (
         tac.rm_code_id AS rawmaterialid,
         current_status.name AS current_status,
         new_status.name AS new_status,
+
+		current_status.id AS current_status_id,
+        new_status.id AS new_status_id,
+		
         SUM(tac.qty_kg) AS total_qty,
         tac.date_computed
 
@@ -276,7 +239,9 @@ WITH ending_balance AS (
         tac.rm_code_id,
         tac.date_computed,
         current_status.name,
-        new_status.name
+        new_status.name,
+		current_status.id,
+		new_status.id
 	),
 
 
@@ -285,6 +250,8 @@ WITH ending_balance AS (
         hf.rm_code_id AS rawmaterialid,
         current_status.name AS current_status,
         new_status.name AS new_status,
+		current_status.id AS current_status_id,
+        new_status.id AS new_status_id,
         SUM(hf.qty_kg) AS total_qty,
         hf.date_computed
 
@@ -302,11 +269,10 @@ WITH ending_balance AS (
         hf.rm_code_id,
         hf.date_computed,
         current_status.name,
-        new_status.name
-
+        new_status.name,
+		current_status.id,
+        new_status.id
 	),
-
-	
 
 
 -- ---------------------------[Computation Queries]---------------------------
@@ -377,7 +343,70 @@ WITH ending_balance AS (
 
 
 
-				
+				-- ----------[TRANSFER FORM CORRECT AND INCORRECT RECORDS COMPUTATION]----------
+				+ COALESCE(
+							CASE
+								WHEN tf_whfrom_incorrect.status::text = 'held : reject'::text THEN tf_whfrom_incorrect.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN tf_whto_incorrect.status::text = 'held : reject'::text THEN tf_whto_incorrect.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+				- COALESCE(
+							CASE
+								WHEN tf_whfrom_correct.status::text = 'held : reject'::text THEN tf_whfrom_correct.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				+ COALESCE(
+							CASE
+								WHEN tf_whto_correct.status::text = 'held : reject'::text THEN tf_whto_correct.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+				-- ----------[CHANGE STATUS FORM CORRECT AND INCORRECT RECORDS COMPUTATION]----------
+				+ COALESCE(
+							CASE
+								WHEN cs_current_incorrect.current_status::text = 'held : reject'::text THEN cs_current_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN cs_new_incorrect.new_status::text = 'held : reject'::text THEN cs_new_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+
+				- COALESCE(
+							CASE
+								WHEN cs_current_incorrect.current_status::text = 'held : reject'::text THEN cs_current_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				+ COALESCE(
+							CASE
+								WHEN cs_new_incorrect.new_status::text = 'held : reject'::text THEN cs_new_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+
 				WHEN eb.status::text = 'held : contaminated'::text 
 				THEN 
 				- COALESCE(
@@ -435,7 +464,70 @@ WITH ending_balance AS (
 
 
 
-				
+
+
+				-- ----------[TRANSFER FORM CORRECT AND INCORRECT RECORDS COMPUTATION]----------
+				+ COALESCE(
+							CASE
+								WHEN tf_whfrom_incorrect.status::text = 'held : contaminated'::text THEN tf_whfrom_incorrect.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN tf_whto_incorrect.status::text = 'held : contaminated'::text THEN tf_whto_incorrect.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+				- COALESCE(
+							CASE
+								WHEN tf_whfrom_correct.status::text = 'held : contaminated'::text THEN tf_whfrom_correct.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				+ COALESCE(
+							CASE
+								WHEN tf_whto_correct.status::text = 'held : contaminated'::text THEN tf_whto_correct.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+				-- ----------[CHANGE STATUS FORM CORRECT AND INCORRECT RECORDS COMPUTATION]----------
+				+ COALESCE(
+							CASE
+								WHEN cs_current_incorrect.current_status::text = 'held : contaminated'::text THEN cs_current_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN cs_new_incorrect.new_status::text = 'held : contaminated'::text THEN cs_new_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN cs_current_incorrect.current_status::text = 'held : contaminated'::text THEN cs_current_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+				+ COALESCE(
+							CASE
+								WHEN cs_new_incorrect.new_status::text = 'held : contaminated'::text THEN cs_new_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
 
 				WHEN eb.status::text = 'held : under evaluation'::text 
 				THEN 
@@ -492,6 +584,70 @@ WITH ending_balance AS (
                         WHEN pf_correct.statusname::text = 'held : under evaluation'::text THEN COALESCE(pf_correct.total_prepared, 0::numeric) - COALESCE(pf_correct.total_returned, 0::numeric)
                         ELSE NULL::numeric
                     END, 0::numeric) 
+
+
+
+				-- ----------[TRANSFER FORM CORRECT AND INCORRECT RECORDS COMPUTATION]----------
+				+ COALESCE(
+							CASE
+								WHEN tf_whfrom_incorrect.status::text = 'held : under evaluation'::text THEN tf_whfrom_incorrect.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN tf_whto_incorrect.status::text = 'held : under evaluation'::text THEN tf_whto_incorrect.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+				- COALESCE(
+							CASE
+								WHEN tf_whfrom_correct.status::text = 'held : under evaluation'::text THEN tf_whfrom_correct.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				+ COALESCE(
+							CASE
+								WHEN tf_whto_correct.status::text = 'held : under evaluation'::text THEN tf_whto_correct.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+
+				-- ----------[CHANGE STATUS FORM CORRECT AND INCORRECT RECORDS COMPUTATION]----------
+				+ COALESCE(
+							CASE
+								WHEN cs_current_incorrect.current_status::text = 'held : under evaluation'::text THEN cs_current_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN cs_new_incorrect.new_status::text = 'held : under evaluation'::text THEN cs_new_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN cs_current_incorrect.current_status::text = 'held : under evaluation'::text THEN cs_current_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+				+ COALESCE(
+							CASE
+								WHEN cs_new_incorrect.new_status::text = 'held : under evaluation'::text THEN cs_new_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
 
 
 					
@@ -553,6 +709,70 @@ WITH ending_balance AS (
 
 
 
+				-- ----------[TRANSFER FORM CORRECT AND INCORRECT RECORDS COMPUTATION]----------
+				+ COALESCE(
+							CASE
+								WHEN tf_whfrom_incorrect.status::text = 'good'::text THEN tf_whfrom_incorrect.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN tf_whto_incorrect.status::text = 'good'::text THEN tf_whto_incorrect.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+				- COALESCE(
+							CASE
+								WHEN tf_whfrom_correct.status::text = 'good'::text THEN tf_whfrom_correct.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				+ COALESCE(
+							CASE
+								WHEN tf_whto_correct.status::text = 'good'::text THEN tf_whto_correct.transferred_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+
+				-- ----------[CHANGE STATUS FORM CORRECT AND INCORRECT RECORDS COMPUTATION]----------
+				+ COALESCE(
+							CASE
+								WHEN cs_current_incorrect.current_status::text = 'good'::text THEN cs_current_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN cs_new_incorrect.new_status::text = 'good'::text THEN cs_new_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+				- COALESCE(
+							CASE
+								WHEN cs_current_incorrect.current_status::text = 'good'::text THEN cs_current_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+				+ COALESCE(
+							CASE
+								WHEN cs_new_incorrect.new_status::text = 'good'::text THEN cs_new_incorrect.total_qty
+								ELSE NULL::numeric
+							END, 0::numeric
+				)
+
+
+
 				ELSE NULL::numeric
 			END AS adjusted_ending_balance,
 			
@@ -566,6 +786,9 @@ WITH ending_balance AS (
 			 	AND eb.rawmaterialid = spillage.rawmaterialid 
 			 	AND eb.statusid = spillage.statusid
 
+
+				 
+	-- ----------[LEFT JOIN FOR RECEIVING FORM TABLE]----------
 		LEFT JOIN adf_receiving_correct rr_correct 
 			ON eb.warehouseid = rr_correct.warehouseid 
 			 	AND eb.rawmaterialid = rr_correct.rawmaterialid 
@@ -577,6 +800,8 @@ WITH ending_balance AS (
 				AND eb.statusid = rr_incorrect.statusid
 
 
+
+	-- ----------[LEFT JOIN FOR OUTGOING FORM TABLE]----------
 		LEFT JOIN adf_outgoing_correct ogr_correct 
 			ON eb.warehouseid = ogr_correct.warehouseid 
 				AND eb.rawmaterialid = ogr_correct.rawmaterialid 
@@ -587,6 +812,9 @@ WITH ending_balance AS (
 				AND eb.rawmaterialid = ogr_incorrect.rawmaterialid 
 				AND eb.statusid = ogr_incorrect.statusid
 
+
+
+	-- ----------[LEFT JOIN FOR PREPARATION FORM TABLE]----------
 		LEFT JOIN adf_preparation_correct pf_correct 
 			ON eb.warehouseid = pf_correct.warehouseid 
 				AND eb.rawmaterialid = pf_correct.rawmaterialid 
@@ -598,10 +826,52 @@ WITH ending_balance AS (
 				AND eb.statusid = pf_incorrect.statusid
 
 		
-			 
+	-- ----------[LEFT JOIN FOR TRANSFER FORM TABLE]----------
+		LEFT JOIN adf_transfer_incorrect tf_whfrom_incorrect 
+			ON eb.warehouseid = tf_whfrom_incorrect.wh_from_id 
+				AND eb.rawmaterialid = tf_whfrom_incorrect.rawmaterialid 
+				AND eb.statusid = tf_whfrom_incorrect.statusid
+
+		LEFT JOIN adf_transfer_incorrect tf_whto_incorrect 
+			ON eb.warehouseid = tf_whto_incorrect.wh_to_id 
+				AND eb.rawmaterialid = tf_whto_incorrect.rawmaterialid 
+				AND eb.statusid = tf_whto_incorrect.statusid
+				
+		LEFT JOIN adf_transfer_correct	tf_whfrom_correct 
+			ON eb.warehouseid = tf_whfrom_correct.wh_from_id 
+				AND eb.rawmaterialid = tf_whfrom_correct .rawmaterialid 
+				AND eb.statusid = tf_whfrom_correct.statusid
+
+		LEFT JOIN adf_transfer_correct tf_whto_correct 
+			ON eb.warehouseid = tf_whto_correct.wh_to_id 
+				AND eb.rawmaterialid = tf_whto_correct .rawmaterialid 
+				AND eb.statusid = tf_whto_correct.statusid
+
+
+	-- ----------[LEFT JOIN FOR CHANGE STATUS FORM TABLE]----------
+		LEFT JOIN adf_change_status_incorrect cs_current_incorrect 
+			ON eb.warehouseid = cs_current_incorrect.warehouseid 
+				AND eb.rawmaterialid = cs_current_incorrect.rawmaterialid 
+				AND eb.statusid = cs_current_incorrect.current_status_id
+
+		LEFT JOIN adf_change_status_incorrect cs_new_incorrect 
+			ON eb.warehouseid = cs_new_incorrect.warehouseid 
+				AND eb.rawmaterialid = cs_new_incorrect.rawmaterialid 
+				AND eb.statusid = cs_new_incorrect.new_status_id
+				
+		LEFT JOIN adf_change_status_correct cs_current_correct 
+			ON eb.warehouseid = cs_current_correct.warehouseid 
+				AND eb.rawmaterialid = cs_current_correct.rawmaterialid 
+				AND eb.statusid = cs_current_correct.current_status_id
+
+		LEFT JOIN adf_change_status_correct cs_new_correct 
+			ON eb.warehouseid = cs_new_correct.warehouseid 
+				AND eb.rawmaterialid = cs_new_correct.rawmaterialid 
+				AND eb.statusid = cs_new_correct.new_status_id
+
+
 	  ORDER BY eb.rmcode, eb.warehousename, eb.warehousenumber, (COALESCE(eb.status, ''::character varying)) NULLS FIRST
 	)
-
 
 
  SELECT rawmaterialid,
