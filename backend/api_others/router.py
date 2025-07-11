0bea06570b802535eed635bc88aa740b42e454b5
MBPI-Francis <mbpi.francis@gmail.com>
2025-07-11 10:19:20 +0800

    Commits

--- backend/api_others/router.py
+++ backend/api_others/router.py
@@ -415,7 +415,6 @@ async def check_stock_value(rm_id: UUID,
             else:
                 return True
 
-
         else:
             return False
     except Exception as e:
@@ -487,17 +486,22 @@ async def check_stock_for_update(rm_id: UUID,
 
         ending_balance_result = db.execute(ending_balance_query)
         ending_balance = ending_balance_result.fetchone()
-
+        # 300
 
         if float(prev_entered_qty) < 0:
+            # -300
+            print(float(prev_entered_qty))
             conv_positive_value = abs(float(prev_entered_qty))
+            # +300
             result_of_beginning_and_entered_qty = float(conv_positive_value) + float(ending_balance[0])
-
+        #     600
+            print("THIS IS THE RESULT: ", result_of_beginning_and_entered_qty)
 
         else:
             result_of_beginning_and_entered_qty = float(ending_balance[0]) - float(prev_entered_qty)
 
         ending_result = new_entered_qty + result_of_beginning_and_entered_qty
+        # 1100
         if ending_result < 0:
             return False
 
